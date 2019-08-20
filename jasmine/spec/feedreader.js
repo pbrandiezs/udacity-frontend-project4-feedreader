811a442bbbbe577980e041da37ffb4f14b58f9ae
Perry Brandiezs <pbrandiezs@gmail.com>
2019-08-19 17:40:36 -0700

    feat: Added test for feed name

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,7 +31,7 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-        it('each feed is defined and url is not empty', function(){
+        it('each feed url is defined and url is not empty', function(){
             for (feed of allFeeds) {
                 expect(feed.url).toBeDefined();
                 expect(feed.url.length).not.toBe(0);
@@ -42,6 +42,12 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('each feed name is defined and name is not empty', function(){
+            for (feed of allFeeds) {
+                expect(feed.name).toBeDefined();
+                expect(feed.name.length).not.toBe(0);
+            };
+        });
     });
 
 
